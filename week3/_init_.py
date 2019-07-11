08f0c83ba94bd4b53ab80711b3d5148ae7a6d565
张裕童 <zhangyutong@zhangyutongdeMacBook-Air.local>
2019-07-11 19:56:59 +1000

    week 3

---
+++ week3/_init_.py
@@ -0,0 +1,24 @@
+#for loops
+my_list = [34,10,55,11]
+for value in my_list:
+    print(value)
+
+#range
+for idx in range(5):
+    print(idx)
+
+for idx in range(len(my_list)):
+    print(my_list[idx])
+
+
+#while loops
+x = 10
+y = 10
+while y >= x:
+    print(my_list[0])
+
+#while loops
+idx = 0
+while idx < len(my_list):
+    print(my_list[idx])
+    idx = idx + 1
--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,13 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    my_list = [ ]
+    my_number = start
+    while my_number < stop:
+        my_list.append(my_number)
+        my_number += step
+    return my_list
+         
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +26,10 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    my_list = []
+    for i in range(start, stop, step):
+            my_list.append(i)
+    return my_list
 
 
 def two_step_ranger(start, stop):
@@ -29,8 +38,13 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
-
+    my_list = []
+    for i in range(start, stop):
+            my_list.append(i)
+            i +=2
+    return my_list
+  
+  
 
 def stubborn_asker(low, high):
     """Ask for a number between low and high until actually given one.
